310d74e175314bf84dde0d0de9fe5d1c7a7f3bfc
Heeae <chromeee@daum.net>
2022-10-16 22:43:26 +0900

    data fetch

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,9 +1,62 @@
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.div`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${({ theme }) => theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
 
 function Coin() {
+  const [loading, setLoading] = useState(true);
+  const [info, setInfo] = useState({});
+  const [priceInfo, setPriceInfo] = useState({});
   const { coinId } = useParams();
+  const { state } = useLocation();
 
-  return <div>Coin : {coinId}</div>;
+  useEffect(() => {
+    (async () => {
+      // coin info
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+
+      //coin price
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+
+      setInfo(infoData);
+      setPriceInfo(priceData);
+    })();
+  }, [coinId]);
+
+  return (
+    <Container>
+      <Header>
+        <Title>코인 : {state?.name ?? "loading..."}</Title>
+      </Header>
+      {loading ? <Loader>"loading..."</Loader> : null}
+    </Container>
+  );
 }
 
 export default Coin;
